30e113f19c344f738244cdf0018af569ccb2d0da
Moxiu9s <natanessa99@gmail.com>
2024-10-15 17:07:28 +0200

    Add files via upload

---
+++ AdbIn10secounds_Installer.bat.bat
@@ -0,0 +1,17 @@
+@echo off
+:: Check if Python is installed
+where python >nul 2>nul
+if %errorlevel% neq 0 (
+    echo Python is not installed. Please install Python and add it to your PATH.
+    pause
+    exit /b
+)
+
+:: Change to the directory where your script is located
+cd /d "%~dp0"
+
+:: Run the Python script
+python AdbIn10secounds_Installer.py
+
+:: Pause to keep the window open
+pause
---
+++ README.txt
@@ -0,0 +1,5 @@
+You Can Use "exe-based" for simplicy just click run installer scrpit and done
+
+if you decide to use original_app folder Run .bat File to Avoid Any ISSUES!
+
+made by: https://github.com/Moxiu9s
